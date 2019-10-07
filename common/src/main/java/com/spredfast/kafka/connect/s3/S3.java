be8ab55b1f04caa0186f8873f6f3e768933f6094
Peter Crossley <pete.crossley@oracle.com>
2019-10-07 10:39:06 -0700

    updated new version of kafka and aws sdk

--- common/src/main/java/com/spredfast/kafka/connect/s3/S3.java
+++ common/src/main/java/com/spredfast/kafka/connect/s3/S3.java
@@ -3,26 +3,32 @@ package com.spredfast.kafka.connect.s3;
 import java.util.Map;
 import java.util.Objects;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.S3ClientOptions;
 
 public class S3 {
 
 	public static AmazonS3 s3client(Map<String, String> config) {
 		// Use default credentials provider that looks in Env + Java properties + profile + instance role
-		AmazonS3 s3Client = new AmazonS3Client();
+		AmazonS3ClientBuilder s3Client = AmazonS3ClientBuilder.standard();
 
 		// If worker config sets explicit endpoint override (e.g. for testing) use that
 		String s3Endpoint = config.get("s3.endpoint");
-		if (s3Endpoint != null && !Objects.equals(s3Endpoint, "")) {
-			s3Client.setEndpoint(s3Endpoint);
+		String s3Region = config.get("s3.region");
+		if ((s3Endpoint != null && !Objects.equals(s3Endpoint, "")) && s3Region != null && !Objects.equals(s3Region, "")) {
+			s3Client.setEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(s3Endpoint, s3Region));
 		}
 		Boolean s3PathStyle = Boolean.parseBoolean(config.get("s3.path_style"));
 		if (s3PathStyle) {
-			s3Client.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
+			s3Client.setPathStyleAccessEnabled(true);
+
 		}
-		return s3Client;
+		return s3Client.build();
 	}
 
 }
